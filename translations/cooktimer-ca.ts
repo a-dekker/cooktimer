44542aa88bdc020d82fbb8b2ebec9795b008264b
Arno Dekker <adekker@gmail.com>
2015-07-23 21:18:10 +0200

    Version 0.1-26

---
+++ translations/cooktimer-ca.ts
@@ -0,0 +1,207 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="19"/>
+        <source>About</source>
+        <translation>Sobre l&apos;aplicació</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="22"/>
+        <source>Info</source>
+        <translation>Informació</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="54"/>
+        <source>Version</source>
+        <translation>Versió</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="59"/>
+        <source>&quot;Timers for cooking&quot;</source>
+        <translation>&quot;Rellotges de cuina&quot;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="65"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+</context>
+<context>
+    <name>DishEdit</name>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="23"/>
+        <source>Save</source>
+        <translation>Desar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="24"/>
+        <source>Cancel</source>
+        <translation>Cancel·lar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="54"/>
+        <source>Deleting</source>
+        <translation>Esborrant</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="64"/>
+        <source>Dish name</source>
+        <translation>Nom del Plat</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="81"/>
+        <source>Hours</source>
+        <translation>Hores</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="108"/>
+        <source>minutes</source>
+        <translation>Minuts</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="136"/>
+        <source>seconds</source>
+        <translation>Segons</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="163"/>
+        <source>No dishes defined. Press the plus button to add one.</source>
+        <translation>Cap Plat definit. Prem el botó &quot;més&quot; per afegir-ne un.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DishEdit.qml" line="174"/>
+        <source>New dish</source>
+        <translation>Nou Plat</translation>
+    </message>
+</context>
+<context>
+    <name>DishPage</name>
+    <message>
+        <location filename="../qml/pages/DishPage.qml" line="88"/>
+        <source>No dishes defined. Choose &quot;Edit Dishes&quot; from the pulley menu.</source>
+        <translation>Cap Plat definit. Selecciona &quot;Editar Plats&quot; des del menú extensible.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="258"/>
+        <location filename="../qml/pages/MainPage.qml" line="262"/>
+        <source>ready</source>
+        <translation>Llest</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="362"/>
+        <source>About</source>
+        <translation>Sobre l&apos;aplicació</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="366"/>
+        <source>Settings</source>
+        <translation>Ajustaments</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="370"/>
+        <source>Edit dishes</source>
+        <translation>Editar Plats</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="405"/>
+        <location filename="../qml/pages/MainPage.qml" line="624"/>
+        <location filename="../qml/pages/MainPage.qml" line="840"/>
+        <source>Dish</source>
+        <translation>Plat</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="454"/>
+        <location filename="../qml/pages/MainPage.qml" line="456"/>
+        <location filename="../qml/pages/MainPage.qml" line="672"/>
+        <location filename="../qml/pages/MainPage.qml" line="674"/>
+        <location filename="../qml/pages/MainPage.qml" line="888"/>
+        <location filename="../qml/pages/MainPage.qml" line="890"/>
+        <source>Stop</source>
+        <translation>Aturar </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="455"/>
+        <location filename="../qml/pages/MainPage.qml" line="673"/>
+        <location filename="../qml/pages/MainPage.qml" line="889"/>
+        <source>Start</source>
+        <translation>Iniciar</translation>
+    </message>
+</context>
+<context>
+    <name>SettingPage</name>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="44"/>
+        <source>Save</source>
+        <translation>Desar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="45"/>
+        <source>Cancel</source>
+        <translation>Cancel·lar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="49"/>
+        <source>Settings</source>
+        <translation>Ajustaments</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="55"/>
+        <source>Keep backlight on</source>
+        <translation>Mantenir la retroiluminació</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="56"/>
+        <source>Prevent screen from dimming.</source>
+        <translation>Evita que s&apos;apagui la pantalla.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="62"/>
+        <source>Show additional banner</source>
+        <translation>Mostrar panell adicional</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="63"/>
+        <source>Notification banner in upper screen.</source>
+        <translation>Panell de notificacions a la part superior de la pantalla.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="81"/>
+        <source>Language:</source>
+        <translation>Llengua:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="227"/>
+        <source>Change of language will be active after restarting the application.</source>
+        <translation>El canvi de llengua es farà efectiu un cop reinicieu l&apos;aplicació.</translation>
+    </message>
+</context>
+<context>
+    <name>TimeDialog</name>
+    <message>
+        <location filename="../qml/pages/TimeDialog.qml" line="50"/>
+        <source>Save</source>
+        <translation>Desar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimeDialog.qml" line="51"/>
+        <source>Cancel</source>
+        <translation>Cancel·lar </translation>
+    </message>
+</context>
+<context>
+    <name>cooktimer</name>
+    <message>
+        <location filename="../qml/cooktimer.qml" line="12"/>
+        <location filename="../qml/cooktimer.qml" line="13"/>
+        <location filename="../qml/cooktimer.qml" line="14"/>
+        <source>Dish</source>
+        <translation>Plat</translation>
+    </message>
+</context>
+</TS>
